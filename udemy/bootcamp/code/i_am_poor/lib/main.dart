bc2bb5b50e93d518f414dc1cffab0da5ae865108
Aaron Guo <aaronguostudio@gmail.com>
2019-07-14 12:47:06 -0600

    add i-am-poor app

---
+++ udemy/bootcamp/code/i_am_poor/lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(
+  MaterialApp(
+    home: Scaffold(
+      backgroundColor: Colors.blueGrey[500],
+      appBar: AppBar(
+        title: Text("I AM POOR"),
+        backgroundColor: Colors.blueGrey[900],
+      ),
+      body: Center(
+        child: Image(
+          image: AssetImage("images/hugo-delete-confirmation.png"),
+        ),
+      )
+    ),
+  )
+);
