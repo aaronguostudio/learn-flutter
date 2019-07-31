9aa5ff53ff9af18b0e31ae783a88a19eda47cff2
aaronguostudio <aaron.guo@payload.com>
2019-07-31 07:26:24 -0600

    add bitcoin tracker app

---
+++ udemy/bootcamp/bitcoin-ticker-flutter-master/lib/price_screen.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:bitcoin_ticker/coin_data.dart';
+
+class PriceScreen extends StatefulWidget {
+  @override
+  _PriceScreenState createState() => _PriceScreenState();
+}
+
+class _PriceScreenState extends State<PriceScreen> {
+
+  String selectedCurrency = 'USD';
+
+  List<DropdownMenuItem<String>> getDropdownItems () {
+    List<DropdownMenuItem<String>> list = [];
+    for (String currency in currenciesList) {
+      list.add(DropdownMenuItem(child: Text(currency), value: currency,));
+    }
+    return list;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('🤑 Coin Ticker'),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Padding(
+            padding: EdgeInsets.fromLTRB(18.0, 18.0, 18.0, 0),
+            child: Card(
+              color: Colors.lightBlueAccent,
+              elevation: 5.0,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10.0),
+              ),
+              child: Padding(
+                padding: EdgeInsets.symmetric(vertical: 15.0, horizontal: 28.0),
+                child: Text(
+                  '1 BTC = ? USD',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 20.0,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+            ),
+          ),
+          Container(
+            height: 150.0,
+            alignment: Alignment.center,
+            padding: EdgeInsets.only(bottom: 30.0),
+            color: Colors.lightBlue,
+            child: CupertinoPicker(
+              backgroundColor: Colors.blue,
+              itemExtent: 32.0, onSelectedItemChanged: (selectedIndex) {
+              print(selectedIndex);
+            }, children: currenciesList.map((label) => Text(label)).toList()),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+
+// Material Style
+//DropdownButton<String>(
+//value: selectedCurrency,
+//items: getDropdownItems(), onChanged: (value) {
+//setState(() {
+//selectedCurrency = value;
+//});
+//})
