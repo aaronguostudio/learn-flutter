0352f424606e2e56a4a1f3875acf25c181265be2
Aaron Guo <aaronguostudio@gmail.com>
2019-07-31 21:20:24 -0600

    add flash chat app

---
+++ udemy/bootcamp/flash-chat-flutter-master/lib/screens/login_screen.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+import 'package:flash_chat/components/button.dart';
+import 'package:flash_chat/constants.dart';
+
+class LoginScreen extends StatefulWidget {
+
+  static const String id = 'login_screen';
+
+  @override
+  _LoginScreenState createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 24.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Container(
+              height: 200.0,
+              child: Image.asset('images/logo.png'),
+            ),
+            SizedBox(
+              height: 48.0,
+            ),
+            TextField(
+              onChanged: (value) {
+                //Do something with the user input.
+              },
+              decoration: kInputDecoration.copyWith(
+                hintText: "Enter your email",
+              )
+            ),
+            SizedBox(
+              height: 8.0,
+            ),
+            TextField(
+              onChanged: (value) {
+                //Do something with the user input.
+              },
+              decoration: kInputDecoration.copyWith(
+                hintText: "Enter your password",
+              )
+            ),
+            SizedBox(
+              height: 24.0,
+            ),
+            Button(
+              label: 'Log In',
+              color: Colors.lightBlueAccent,
+              onPressed: () {
+                //
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
